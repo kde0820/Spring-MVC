b05f895f648ba1b53aff2a505ed989629b1f9abc
kde0820 <kde0820@naver.com>
2018-07-20 14:28:47 +0900

    Test Model

---
+++ out/artifacts/spring_mvc_war_exploded/WEB-INF/views/model/modelAndView.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: daeun
+  Date: 2018. 7. 20.
+  Time: PM 2:23
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Model And View Test</title>
+</head>
+<body>
+    ID: ${id}
+</body>
+</html>
--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -1,12 +1,30 @@
 package controller;
 
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
 
 @org.springframework.stereotype.Controller
 public class Controller {
 
     @RequestMapping(value = "/")
-    public String test(){
+    public String index(){
         return "index";
+    }
+
+    @RequestMapping(value = "/model/modelTest")
+    public String modelTest(Model model){
+        model.addAttribute("data", "Test Data");
+        return "/model/modelTest";
+    }
+
+    @RequestMapping(value = "/model/modelAndView")
+    public ModelAndView modelAndView(){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("id","Test ID");
+        modelAndView.setViewName("/model/modelAndView");
+
+        return modelAndView;
     }
 }
