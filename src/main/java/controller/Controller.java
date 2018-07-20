dbdd6579959a6d00c54ca63e1e489767bb0dc4d6
kde0820 <kde0820@naver.com>
2018-07-20 14:04:51 +0900

    Basic Setting

---
+++ src/main/java/controller/Controller.java
@@ -0,0 +1,12 @@
+package controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@org.springframework.stereotype.Controller
+public class Controller {
+
+    @RequestMapping(value = "/")
+    public String test(){
+        return "index";
+    }
+}
